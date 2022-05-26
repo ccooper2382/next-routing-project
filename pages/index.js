842f70324db870df7bad2937624365d9d1f54446
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-05-26 14:19:39 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {getFeaturedEvents} from "../dummy-data";
+import EventList from "../components/events/event-list";
+
+function HomePage() {
+
+    const featuredEvents = getFeaturedEvents();
+
+    return (
+        <div>
+            <h1>The Home Page</h1>
+            <EventList items={featuredEvents}/>
+        </div>
+    );
+}
+
+export default HomePage;
